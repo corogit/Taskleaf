188af0a1751354130510197e0e2d17d5772bd331
corogit <n.ohta5192@gmail.com>
2021-02-07 08:02:36 +0000

    [add]ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # 全てのビューから利用可能にする
+  helper_method :current_user
+  
+  private
+  # 全てのコントローラーから利用可能にする
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
